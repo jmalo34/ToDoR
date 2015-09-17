532e4cd97e11e6b6f975ca38bca7d1651929d8bc
Jenna Maloney <jmalo@uw.edu>
2015-09-17 15:37:32 -0700

    function display as webpage, use relationships in Silex

---
+++ README.md
@@ -0,0 +1,46 @@
+# _{ToDoRList}_
+
+##### _{To-Do-List, with relationships in SQL}, {August 2015}_
+
+#### By _**{Jenna Maloney}**_
+
+## Description
+
+_{This is a detailed description of your application. Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. Do you want other developers to use this code in their apps? Talk about things like that here in simple terms.}_
+
+## Setup
+
+* _clone repository_
+* _run $ composer install_
+* _in a simple_
+* _easy-to-understand_
+* _format_
+
+_{Leave nothing to chance! You want it to be easy for potential employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Are there other applications this app depends on?}_
+
+## Technologies Used
+
+_{HTML, Bootstrap, Silex, composer, PHP, apache, SQL, Twig}_
+
+### Legal
+
+Copyright (c) 2015 **_{Jenna Maloney}_**
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ app/app.php
@@ -0,0 +1,68 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app)
+    {
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->get("/tasks", function() use ($app)
+    {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app)
+    {
+        return $app['twig']->render('category.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->get("/categories/{id}", function ($id) use ($app)
+    {
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+    });
+
+    $app->post("/tasks", function() use ($app)
+    {
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+        $task = new Task($description, $id = null, $category_id);
+        $task->save();
+        $category = Category::find($category_id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
+    });
+
+    $app->post("/categories", function() use ($app)
+    {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_tasks", function() use ($app)
+    {
+        Task::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/delete_categories", function() use ($app)
+    {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+ ?>
